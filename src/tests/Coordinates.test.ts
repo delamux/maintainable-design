e405e6d3f96f8308edfaf741d1543a96c7e75388
Luis del Amo <luisdelamo@gmail.com>
2024-12-09 20:35:51 +0100

    Covering when reach the limit at 9 and 0

--- src/tests/Coordinates.test.ts
+++ src/tests/Coordinates.test.ts
@@ -8,18 +8,30 @@ class Coordinates {
 
 	increaseLatitude() {
 		this.latitude = this.latitude + 1;
+		if (this.latitude >= this.boundaryLatitude) {
+			this.latitude = this.latitude % this.boundaryLatitude;
+		}
 	}
 
 	decreaseLatitude() {
 		this.latitude = this.latitude - 1;
+		if (this.latitude < 0) {
+			this.latitude = this.boundaryLatitude - 1;
+		}
 	}
 
 	increaseLongitude() {
 		this.longitude = this.longitude + 1;
+		if (this.longitude >= this.boundaryLatitude) {
+			this.longitude = this.longitude % this.boundaryLatitude;
+		}
 	}
 
 	decreaseLongitude() {
 		this.longitude = this.longitude - 1;
+		if (this.longitude < 0) {
+			this.longitude = this.boundaryLongitude - 1;
+		}
 	}
 }
 
@@ -43,5 +55,25 @@ describe('Coordinates', () => {
 		const coordinates = new Coordinates(0, 2);
 		coordinates.decreaseLongitude();
 		expect(coordinates).toEqual(new Coordinates(0, 1));
+	});
+	it('Should return the right increase coordinates when the boundary is reached', () => {
+		const coordinates = new Coordinates(9, 0);
+		coordinates.increaseLatitude();
+		expect(coordinates).toEqual(new Coordinates(0, 0));
+	});
+	it('Should return the right increase longitude coordinates when the boundary is reached', () => {
+		const coordinates = new Coordinates(0, 9);
+		coordinates.increaseLongitude();
+		expect(coordinates).toEqual(new Coordinates(0, 0));
+	});
+	it('Should return the right decrease coordinates when latitude is 0', () => {
+		const coordinates = new Coordinates(0, 0);
+		coordinates.decreaseLatitude();
+		expect(coordinates).toEqual(new Coordinates(9, 0));
+	});
+	it('Should return the right decrease coordinates when longitude is 0', () => {
+		const coordinates = new Coordinates(0, 0);
+		coordinates.decreaseLongitude();
+		expect(coordinates).toEqual(new Coordinates(0, 9));
 	});
 });
